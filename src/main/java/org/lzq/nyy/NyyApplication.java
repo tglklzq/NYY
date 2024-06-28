0ff198bff02905cfdf92059c9664cef3e1d2f745
liangzhiquan <1298848072@qq.com>
2024-06-28 14:25:34 +0800

    项目启动

---
+++ src/main/java/org/lzq/nyy/NyyApplication.java
@@ -0,0 +1,13 @@
+package org.lzq.nyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyyApplication.class, args);
+    }
+
+}
