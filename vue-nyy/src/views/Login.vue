d0df3f9c0062e33f987c41c099298326c801406f
liangzhiquan <1298848072@qq.com>
2024-07-01 00:02:07 +0800

    样式更改

--- vue-nyy/src/views/Login.vue
+++ vue-nyy/src/views/Login.vue
@@ -1,113 +1,78 @@
 <template>
-  <a-form
-      :model="formState"
-      name="basic"
-      :label-col="{ span: 8 }"
-      :wrapper-col="{ span: 16 }"
-      autocomplete="off"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-      id="login-form"
-  >
-    <!-- 登录表单项 -->
-    <a-form-item
-        label="邮箱"
-        name="email"
-        :rules="[{ required: true, message: '请输入邮箱!' }]"
-    >
-      <a-input v-model:value="formState.email" />
-    </a-form-item>
+  <div class="outer-container">
+    <div class="form-container">
+      <a-form
+          ref="formRef"
+          name="basic"
+          :model="formState"
+          v-bind="layout"
+          labelAlign='right'
+          autocomplete="off"
 
-    <a-form-item
-        label="密码"
-        name="passwordHash"
-        :rules="[{ required: true, message: '请输入密码!' }]"
-    >
-      <a-input-password v-model:value="formState.passwordHash" />
-    </a-form-item>
+          @finish="onFinish"
+          @finishFailed="onFinishFailed"
+          id="login-form"
+          class="custom-form"
 
-    <!-- 注册表单项，初始状态隐藏 -->
-    <a-form-item
-        v-if="showRegisterForm"
-        label="用户名称"
-        name="username"
-        :rules="[{ required: true, message: '请输入用户名称!' }]"
-    >
-      <a-input v-model:value="formState.username" />
-    </a-form-item>
+      >
+        <!-- 登录表单项 -->
+        <a-form-item label="邮箱" name="email" :rules="[{ required: true, message: '请输入邮箱!' }]">
+          <a-input v-model:value="formState.email" class="custom-input" />
+        </a-form-item>
 
-    <a-form-item
-        v-if="showRegisterForm"
-        label="手机号"
-        name="phoneNumber"
-        :rules="[{ required: true, message: '请输入手机号!' }]"
-    >
-      <a-input v-model:value="formState.phoneNumber" />
-    </a-form-item>
+        <a-form-item label="密码" name="passwordHash" :rules="[{ required: true, message: '请输入密码!' }]">
+          <a-input-password v-model:value="formState.passwordHash" class="custom-input"/>
+        </a-form-item>
 
-    <!-- 记住密码复选框 -->
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formState.remember">记住密码</a-checkbox>
-    </a-form-item>
 
-    <!-- 按钮行 -->
-    <a-form-item>
-      <a-row type="flex" justify="end">
-        <a-col :span="8">
-          <a-form-item>
-            <a-button
-                v-if="!showRegisterForm"
-                type="primary"
-                html-type="button"
-                @click="onLogin"
-            >
+
+        <!-- 注册表单项，初始状态隐藏 -->
+        <a-form-item v-if="showRegisterForm" label="用户姓名" name="username" :rules="[{ required: true, message: '请输入用户名称!' }]">
+          <a-input v-model:value="formState.username" class="custom-input"/>
+        </a-form-item>
+
+        <a-form-item v-if="showRegisterForm" label="手机号" name="phoneNumber" :rules="[{ required: true, message: '请输入手机号!' }]">
+          <a-input v-model:value="formState.phoneNumber" class="custom-input"/>
+        </a-form-item>
+
+        <!-- 按钮行 -->
+        <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+          <div class="button-container" >
+
+            <a-button v-if="!showRegisterForm" type="primary" html-type="button" @click="onLogin" class="custom-button">
               确定
             </a-button>
-            <a-button
-                v-if="showRegisterForm"
-                type="primary"
-                html-type="button"
-                @click="submitRegistration"
-            >
+            <a-button v-if="showRegisterForm" type="primary" html-type="button" @click="submitRegistration" class="custom-button">
               提交
             </a-button>
-          </a-form-item>
-        </a-col>
-
-        <a-col :span="8">
-          <a-form-item>
-            <a-button
-                v-if="showRegisterForm"
-                type="default"
-                html-type="button"
-                @click="cancelRegistration"
-            >
+            <a-button style="margin-left: 10px" v-if="showRegisterForm" type="default" html-type="button" @click="cancelRegistration" class="custom-button">
               取消
             </a-button>
-          </a-form-item>
-        </a-col>
-
-        <a-col :span="8">
-          <a-form-item>
-            <a-button
-                v-if="!showRegisterForm"
-                type="default"
-                html-type="button"
-                @click="toRegister"
-            >
+            <a-button style="margin-left: 10px" v-if="!showRegisterForm" type="default" html-type="button" @click="toRegister" class="custom-button">
               注册
             </a-button>
-          </a-form-item>
-        </a-col>
-      </a-row>
-    </a-form-item>
-  </a-form>
+
+          </div>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { reactive, ref } from 'vue';
 import { message } from 'ant-design-vue'; // 导入 message 对象
 import axios from 'axios';
+
+const formRef = ref();
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+};
 
 const formState = reactive({
   email: '',
@@ -196,3 +161,27 @@ const resetForm = () => {
 };
 
 </script>
+
+<style scoped>
+.outer-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f2f5;
+}
+
+.form-container {
+  width: 100%;
+  max-width: 500px;
+  padding: 100px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+
+}
+
+
+
+</style>
+
