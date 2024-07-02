3e98b0de936cb90261f72b8a81aefaf999f339f9
liangzhiquan <1298848072@qq.com>
2024-07-02 15:48:48 +0800

    主页

--- vue-nyy/src/views/Layout.vue
+++ vue-nyy/src/views/Layout.vue
@@ -11,33 +11,68 @@
           <img class="logo-icon" src="@/icons/svg/plant.svg" alt="Icon">
         </template>
       </div>
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
-        </a-menu-item>
+
+      <!--菜单-->
+
+      <a-menu
+          v-model:openKeys="openKeys"
+          v-model:selectedKeys="selectedKeys"
+          mode="inline"
+          theme="dark"
+
+      >
+        <a-sub-menu key="sub1">
+          <template #icon>
+            <UserOutlined />
+          </template>
+          <template #title>Navigation One</template>
+          <a-menu-item key="1">Option 5</a-menu-item>
+          <a-menu-item key="2">Option 6</a-menu-item>
+          <a-menu-item key="3">Option 7</a-menu-item>
+          <a-menu-item key="4">Option 8</a-menu-item>
+        </a-sub-menu>
       </a-menu>
+
     </a-layout-sider>
+    <!--顶部-->
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
+      <a-layout-header class="header" >
         <menu-unfold-outlined
             v-if="collapsed"
             class="trigger"
             @click="() => (collapsed = !collapsed)"
         />
-        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+        <menu-fold-outlined
+            v-else class="trigger"
+            @click="() => (collapsed = !collapsed)" />
+        <a-menu v-model:selectedKeys="current" mode="horizontal" theme="dark">
+          <a-menu-item key="mail">
+            <template #icon>
+              <mail-outlined />
+            </template>
+            邮件
+          </a-menu-item>
+          <a-menu-item key="message">
+            <template #icon>
+              <bell-outlined />
+            </template>
+            消息
+          </a-menu-item>
+          <a-sub-menu key="admin">
+            <template #icon>
+              <user-outlined />
+            </template>
+            <template #title>管理员</template>
+              <a-menu-item key="setting:1">个人中心</a-menu-item>
+              <a-menu-item key="setting:2">修改密码</a-menu-item>
+              <a-menu-item key="setting:3">退出登录</a-menu-item>
+          </a-sub-menu>
+
+        </a-menu>
       </a-layout-header>
       <!--主体-->
       <a-layout-content
-          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+          :style="{ margin: '6px', padding: '10px', background: '#fff', minHeight: '280px' }"
       >
         Content
       </a-layout-content>
@@ -47,10 +82,14 @@
 
 <script>
 import { defineComponent, ref } from 'vue';
-import { UserOutlined, VideoCameraOutlined, UploadOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue';
+import { UserOutlined, VideoCameraOutlined, UploadOutlined,
+  MenuUnfoldOutlined, MenuFoldOutlined,BellOutlined,MailOutlined } from '@ant-design/icons-vue';
+
+
 
 export default defineComponent({
   components: {
+    BellOutlined,MailOutlined,
     UserOutlined,
     VideoCameraOutlined,
     UploadOutlined,
@@ -59,7 +98,12 @@ export default defineComponent({
   },
   setup() {
     return {
+      //顶部菜单选中key
+      current:ref(['mail']),
+      //左侧菜单展开的选项
+      openKeys:ref(['sub1']),
       selectedKeys: ref(['1']),
+      //左侧菜单是否折叠
       collapsed: ref(false),
     };
   },
@@ -70,6 +114,13 @@ export default defineComponent({
 .layout {
   width: 100vw;
   height: 100vh;
+  .header{
+    background: #fff;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+    background-color: #001529;
+  }
   .logo-container {
     height: 64px;
     display: flex;
@@ -90,6 +141,17 @@ export default defineComponent({
   .logo-icon {
     width: 32px;
     height: 32px;
+  }
+}
+.trigger{
+  color: white;
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+  &:hover{
+    color: #1890ff;
   }
 }
 </style>
